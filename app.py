58695d06dc5832c8e5f47aec5b7daacf4d1079c4
queirozsc <queirozsc@gmail.com>
2020-04-21 17:17:14 -0300

    Adiciona o Flask Bootstrap

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
     products = ["Ciabata", "Baguete", "Pretzel"]
-    return products[1]
+    return render_template("index.html", **locals())
