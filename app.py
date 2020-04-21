68f94dd0c4a0f5e37452afdec23f6c5b9b4af1af
queirozsc <queirozsc@gmail.com>
2020-04-21 17:33:29 -0300

    Formata usando o Bootstrap

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+app.config['TITLE'] = "Pydaria"
+
 Bootstrap(app)
 
 @app.route('/')
 def index():
     products = ["Ciabata", "Baguete", "Pretzel"]
-    return render_template("index.html", **locals())
+    return render_template("index.html", products=products)
